f9495f38fa301828a01dc2dea6d5abae9b7d3868
leserpent <leserpent@cc2703f2-cfd4-0310-b9fe-ae1eaea9c1e6>
2004-01-11 15:58:22 +0000

    Support for mingw32

---
+++ trunk/sfox/cross-configure.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+PREFIX=/opt/xmingw
+TARGET=i386-mingw32msvc
+PATH="$PREFIX/bin:$PREFIX/$TARGET/bin:$PATH"
+export PATH
+if [ -f "$PREFIX/$TARGET/bin/$TARGET-sdl-config" ]; then
+    SDL_CONFIG="$PREFIX/$TARGET/bin/$TARGET-sdl-config"
+    export SDL_CONFIG
+fi
+cache=cross-config.cache
+sh configure --cache-file="$cache" \
+	--target=$TARGET --host=$TARGET --build=i386-linux \
+	$*
+status=$?
+rm -f "$cache"
+exit $status
