b9aa2e4a8ffe11958476b9a90a8685a2d9d7cb54
leserpent <leserpent@cc2703f2-cfd4-0310-b9fe-ae1eaea9c1e6>
2004-01-28 19:26:57 +0000

    Removed local matrix. Added display list and some opengl's function warper.

--- trunk/sfox/src/starengine/engine/object3d.c
+++ trunk/sfox/src/starengine/engine/object3d.c
@@ -1,5 +1,9 @@
 #ifdef _WIN32
-#include <windows.h>
+# include <windows.h>
+# undef far
+# undef near
+# include <GL/gl.h>
+# include "starglext.h"
 #endif
 
 #include <stdlib.h>
@@ -9,90 +13,140 @@
 
 #include "object3d.h"
 
-struct object3d {
-  matrix4 local;
-  matrix4 world;
-  vertexbuffer vb;
-  material mat;
-};
-
-object3d
-object3d_create(matrix4 local, matrix4 world, vertexbuffer vb, material mat)
+/**********************************************************************/
+/* Init and co                                                        */
+/**********************************************************************/
+object3d *
+object3d_create(matrix4 world, vertexbuffer vb, material mat)
 {
-  object3d obj = malloc(sizeof(struct object3d));
-  assert(obj);
-
-  matrix4_copy(obj->world, world);
-  matrix4_copy(obj->local, local);
-  obj->vb = vb;
-  obj->mat = mat;
+  object3d *obj = malloc(sizeof(struct object3d));
+  object3d_init(obj, world, vb, mat);
 
   return obj;
 }
 
 void
-object3d_set_material(object3d obj, material mat)
+object3d_init(object3d *obj, matrix4 world, vertexbuffer vb, material mat)
 {
+  matrix4_copy(obj->world, world);
+  obj->vb = vb;
   obj->mat = mat;
+
+  if(vb == NULL)
+    obj->list = glGenLists(1);
 }
 
 void
-object3d_to_opengl(object3d obj)
+object3d_destroy(object3d *obj, DestroyFlag flag)
 {
-  assert(obj&&obj->vb);
+  if(obj->vb == NULL)
+    glDeleteLists(obj->list, 1);
+  if(flag&DESTROY_MAT)
+    material_destroy(obj->mat);
+  if(flag&DESTROY_VB)
+    vertexbuffer_destroy(obj->vb);
+}
+
+void
+object3d_free(object3d *obj, DestroyFlag flag)
+{
+  object3d_destroy(obj, flag);
+  free(obj);
+}
+
+/**********************************************************************/
+/* And now for something completely different                         */
+/**********************************************************************/
+void
+object3d_to_opengl(object3d *obj)
+{
+  assert(obj);
   glPushMatrix();		/* Should be somewhere else in a loop */
 
   glMultMatrixd((double *)obj->world);
-  glMultMatrixd((double *)obj->local);
   if(obj->mat)
     material_to_opengl(obj->mat);
-  vertexbuffer_to_opengl(obj->vb);
+  
+  if(obj->vb)
+    vertexbuffer_to_opengl(obj->vb);
+  else
+    glCallList(obj->list);
 
   glPopMatrix();
 }
 
 void
-object3d_destroy(object3d obj, DestroyFlag flag)
+object3d_BeginList(object3d *obj)
 {
-  if(flag&DESTROY_MAT)
-    material_destroy(obj->mat);
-  if(flag&DESTROY_VB)
-    vertexbuffer_destroy(obj->vb);
-  free(obj);
+  assert(!obj->vb);
+  glNewList(obj->list, GL_COMPILE);
+}
+
+void
+object3d_EndList()
+{
+  glEndList();
+}
+
+void
+object3d_Begin(unsigned int type)
+{
+  glBegin(type);
+}
+
+void
+object3d_End()
+{
+  glEnd();
+}
+
+void
+object3d_Vertex3f(float x, float y, float z)
+{
+  glVertex3f(x, y, z);
+}
+
+void
+object3d_TexCoord2f(float u, float v)
+{
+  glTexCoord2f(u, v);
+}
+
+void
+object3d_MultiTexCoord2f(int texunit, float u, float v)
+{
+  glMultiTexCoord2f(GL_TEXTURE0+texunit, u, v);
+}
+
+/************************************************************************/
+/* Getters and Setters                                                  */
+/************************************************************************/
+void
+object3d_set_material(object3d *obj, material mat)
+{
+  obj->mat = mat;
 }
 
 vertexbuffer
-object3d_get_vertexbuffer(object3d obj)
+object3d_get_vertexbuffer(object3d *obj)
 {
   return obj->vb;
 }
 
 material
-object3d_get_material(object3d obj)
+object3d_get_material(object3d *obj)
 {
   return obj->mat;
 }
 
 void
-object3d_set_local_matrix(object3d obj, matrix4 local)
-{
-  matrix4_copy(obj->local, local);
-}
-
-void
-object3d_set_world_matrix(object3d obj, matrix4 world)
+object3d_set_world_matrix(object3d *obj, matrix4 world)
 {
   matrix4_copy(obj->world, world);
 }
 
 matrix4 *
-object3d_get_local_matrix(object3d obj)
-{
-  return &obj->local;
-}
-
-matrix4 *
-object3d_get_world_matrix(object3d obj)
+object3d_get_world_matrix(object3d *obj)
 {
   return &obj->world;
 }
--- trunk/sfox/src/starengine/engine/object3d.h
+++ trunk/sfox/src/starengine/engine/object3d.h
@@ -7,23 +7,42 @@
 
 typedef enum {DESTROY_ALL=0xFF, DESTROY_MAT=2, DESTROY_VB=4} DestroyFlag;
 
-typedef struct object3d *object3d;
+typedef struct object3d {
+  matrix4 world;
+  material mat;
+  
+  vertexbuffer vb;
+  unsigned int list;
+  
+} object3d;
 
-/* loc is copied into object, vb,mat are pointers*/
-extern object3d object3d_create(matrix4 local, matrix4 world, vertexbuffer vb, material mat);
-extern void object3d_destroy(object3d obj, DestroyFlag flag);
 
-extern void object3d_to_opengl(object3d obj);
+/* world is copied into object, vb,mat are pointers*/
+/* if vb is NULL, create an OpenGL list instead */
+extern object3d *object3d_create(matrix4 world, vertexbuffer vb, material mat);
+extern void object3d_init(object3d *obj, matrix4 world, vertexbuffer vb, material mat);
+extern void object3d_destroy(object3d *obj, DestroyFlag flag);
+extern void object3d_free(object3d *obj, DestroyFlag flag);
 
-extern void object3d_set_material(object3d obj, material mat);
-extern material object3d_get_material(object3d obj);
+extern void object3d_to_opengl(object3d *obj);
 
-extern vertexbuffer object3d_get_vertexbuffer(object3d obj);
+extern void object3d_BeginList(object3d *obj);
+extern void object3d_EndList();
+
+/*Warper around OpenGL direct mode*/
+extern void object3d_Begin(unsigned int type);
+extern void object3d_End();
+extern void object3d_TexCoord2f(float u, float v);
+extern void object3d_Vertex3f(float x, float y, float z);
+extern void object3d_MultiTexCoord2f(int texunit, float u, float v);
+
+extern void object3d_set_material(object3d *obj, material mat);
+extern material object3d_get_material(object3d *obj);
+
+extern vertexbuffer object3d_get_vertexbuffer(object3d *obj);
 
 /* All matrices are copied into object3d */
-extern void object3d_set_local_matrix(object3d obj, matrix4 local);
-extern void object3d_set_world_matrix(object3d obj, matrix4 world);
-extern matrix4 *object3d_get_local_matrix(object3d obj);
-extern matrix4 *object3d_get_world_matrix(object3d obj);
+extern void object3d_set_world_matrix(object3d *obj, matrix4 world);
+extern matrix4 *object3d_get_world_matrix(object3d *obj);
 
 #endif
