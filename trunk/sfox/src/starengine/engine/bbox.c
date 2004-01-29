886ac78b70393ae7bec6ae7c36f7538a6f71a005
leserpent <leserpent@cc2703f2-cfd4-0310-b9fe-ae1eaea9c1e6>
2004-01-29 20:12:03 +0000

    bbox2d no longer consider a left or right corner.

--- trunk/sfox/src/starengine/engine/bbox.c
+++ trunk/sfox/src/starengine/engine/bbox.c
@@ -1,12 +1,13 @@
 #include <stdlib.h>
 #include <assert.h>
+#include <math.h>
 
 #include "bbox.h"
 
 void
 bbox2d_set(bbox2d *bb, float tlx, float tly, float brx, float bry)
 {
-  assert(brx>tlx&&bry<tly);
+  assert(brx!=tlx&&bry!=tly);
   
   vector2_set(&bb->corners[0], tlx, tly);
   vector2_set(&bb->corners[1], tlx, bry);
@@ -17,13 +18,13 @@ bbox2d_set(bbox2d *bb, float tlx, float tly, float brx, float bry)
 unsigned int
 bbox2d_width(bbox2d *bb)
 {
-  assert(bb->corners[3].x>bb->corners[0].x);
-  return bb->corners[3].x-bb->corners[0].x;
+  assert(bb->corners[3].x!=bb->corners[0].x);
+  return abs(bb->corners[3].x-bb->corners[0].x);
 }
 
 unsigned int
 bbox2d_height(bbox2d *bb)
 {
-  assert(bb->corners[0].y>bb->corners[1].y);
-  return bb->corners[0].y-bb->corners[1].y;
+  assert(bb->corners[0].y!=bb->corners[1].y);
+  return abs(bb->corners[0].y-bb->corners[1].y);
 }
