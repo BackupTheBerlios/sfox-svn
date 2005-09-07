10c9546db447ee6dd01b66cef5b9d472484a9481
leserpent <leserpent@cc2703f2-cfd4-0310-b9fe-ae1eaea9c1e6>
2005-09-07 17:52:03 +0000

    volrend cg

---
+++ trunk/frameworkedGL/volrendCG/src/testapp.h
@@ -0,0 +1,58 @@
+#ifndef TESTAPP_H
+#define TESTAPP_H
+
+#include "framework/application.h"
+
+
+namespace StarEngine {
+  class FontGL;
+  class Camera;
+  class Texture;
+  class Trackball;
+  class FramebufferObject;
+  class ShaderCG;
+}
+
+class TestApp : public StarEngine::Application {
+private:
+  StarEngine::Trackball *trackball;
+  StarEngine::FontGL *font;
+  StarEngine::Camera *cam;
+  StarEngine::Texture *texture;
+  StarEngine::FramebufferObject *fbo;
+  StarEngine::ShaderCG *shaderVertPass1;
+  StarEngine::ShaderCG *shaderFragPass1;
+  StarEngine::ShaderCG *shaderVertPass2;
+  StarEngine::ShaderCG *shaderFragPass2;
+  StarEngine::ShaderCG *shaderVertPass3;
+  StarEngine::ShaderCG *shaderFragPass3;
+  StarEngine::ShaderCG *shaderVertPass4;
+  StarEngine::ShaderCG *shaderFragPass4;
+
+  static const int cubeX = 2;
+  static const int cubeY = 2;
+  static const int cubeZ = 1;
+
+  void printInfos();
+  void drawCube(float sizex, float sizey, float sizez);
+  void computeRays();
+  void moveOnRay(float dt);
+
+public:
+  TestApp();
+  ~TestApp() {}
+
+  void init();
+  void quit();
+  void render();
+  void idle();
+
+  void keyDown(const SDL_keysym &key);
+  void keyUp(const SDL_keysym &key);
+
+  void mouseButtonDown(const SDL_MouseButtonEvent &m);
+  void mouseButtonUp(const SDL_MouseButtonEvent &m);
+  void mouseMotion(const SDL_MouseMotionEvent &motion);
+};
+
+#endif
