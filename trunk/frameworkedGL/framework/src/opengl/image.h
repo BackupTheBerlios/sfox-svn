1497e56d76c00658566ba7e676cc1db8c6bf59c1
leserpent <leserpent@cc2703f2-cfd4-0310-b9fe-ae1eaea9c1e6>
2005-11-06 18:30:39 +0000

    mipmap

--- trunk/frameworkedGL/framework/src/opengl/image.h
+++ trunk/frameworkedGL/framework/src/opengl/image.h
@@ -17,6 +17,7 @@ namespace StarEngine {
     void flipY();
     void flipX();
     ImageLoader::ImageData *getImageData() { return imgData; }
+    void setImageData(ImageLoader::ImageData *img) { imgData = img; }
     int getDimension() {
       return (imgData->width>1)+(imgData->height>1)+(imgData->depth>1);
     }
