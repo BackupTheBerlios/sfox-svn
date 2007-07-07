11d1e2d93417b42265ebcc04058b1349d7777d1a
leserpent <leserpent@cc2703f2-cfd4-0310-b9fe-ae1eaea9c1e6>
2007-07-07 14:33:02 +0000

    Singleton template

---
+++ trunk/GLstats/src/Singleton.h
@@ -0,0 +1,31 @@
+#ifndef SINGLETON_H
+#define SINGLETON_H
+
+template <typename T>
+class Singleton
+{
+public:
+  static T* getInstance()
+  {
+    if(!m_instance)
+      m_instance = new T;
+    return m_instance;
+  }
+
+  static void destroy()
+  {
+    delete m_instance;
+    m_instance = NULL;
+  }
+
+protected:
+  Singleton() {}
+  virtual ~Singleton() {}
+
+private:
+  static T* m_instance;
+};
+
+template <typename T> T* Singleton<T>::m_instance = NULL;
+
+#endif
